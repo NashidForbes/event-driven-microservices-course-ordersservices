520f0389534a19ac77a2f90727895781fcccf64b
NashidForbes <NashidForbes@biohumans.com>
2021-12-28 19:59:23 -0500

    Creating ReserveProductCommand in sagacoreapi module. Creating a publish reserve command and adding sagacoreapi module as a dependency to OrderServices module for ReserveProductCommand.

--- src/main/java/com/appsdeveloperblog/estore/ordersservice/saga/OrderSaga.java
+++ src/main/java/com/appsdeveloperblog/estore/ordersservice/saga/OrderSaga.java
@@ -1,6 +1,10 @@
 package com.appsdeveloperblog.estore.ordersservice.saga;
 
 import com.appsdeveloperblog.estore.ordersservice.core.events.OrderCreatedEvent;
+import com.appsdeveloperblog.estore.sagacoreapi.commands.ReserveProductCommand;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
@@ -17,6 +21,22 @@ public class OrderSaga {
     @StartSaga
     @SagaEventHandler(associationProperty="orderId")
     public void handle(OrderCreatedEvent orderCreatedEvent){
+        ReserveProductCommand reserveProductCommand = ReserveProductCommand.builder()
+                .orderId(orderCreatedEvent.getOrderId())
+                .productId(orderCreatedEvent.getProductId())
+                .quantity(orderCreatedEvent.getQuantity())
+                .userId(orderCreatedEvent.getUserId())
+                .build();
 
+        commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
+
+            @Override
+            public void onResult(CommandMessage<? extends ReserveProductCommand> commandMessage,
+                                 CommandResultMessage<?> commandResultMessage) {
+                if(commandResultMessage.isExceptional()){
+                    // Start a compensating transaction
+                }
+            }
+        });
     }
 }
