10bcdc055cd89ad60aa342920b06683600badb53
NashidForbes <NashidForbes@biohumans.com>
2022-01-03 02:22:51 -0500

    Handle the OrderApprovedEvent and update the Orders database.

--- src/main/java/com/appsdeveloperblog/estore/ordersservice/core/data/interfaces/OrdersRepository.java
+++ src/main/java/com/appsdeveloperblog/estore/ordersservice/core/data/interfaces/OrdersRepository.java
@@ -4,4 +4,5 @@ import com.appsdeveloperblog.estore.ordersservice.core.data.domains.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrdersRepository extends JpaRepository<OrderEntity, String> {
+    OrderEntity findByOrderId(String orderId);
 }
--- src/main/java/com/appsdeveloperblog/estore/ordersservice/core/handlers/OrderEventsHandler.java
+++ src/main/java/com/appsdeveloperblog/estore/ordersservice/core/handlers/OrderEventsHandler.java
@@ -2,12 +2,15 @@ package com.appsdeveloperblog.estore.ordersservice.core.handlers;
 
 import com.appsdeveloperblog.estore.ordersservice.core.data.domains.OrderEntity;
 import com.appsdeveloperblog.estore.ordersservice.core.data.interfaces.OrdersRepository;
+import com.appsdeveloperblog.estore.ordersservice.core.events.OrderApprovedEvent;
 import com.appsdeveloperblog.estore.ordersservice.core.events.OrderCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @ProcessingGroup("order-group")
 public class OrderEventsHandler {
@@ -20,15 +23,28 @@ public class OrderEventsHandler {
 
 
     @EventHandler
-    public void on(OrderCreatedEvent event){
+    public void on(OrderCreatedEvent event) {
         OrderEntity orderEntity = new OrderEntity();
         BeanUtils.copyProperties(event, orderEntity);
 
         try {
             this.ordersRepository.save(orderEntity);
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
 
+    @EventHandler
+    public void on(OrderApprovedEvent orderApprovedEvent) {
+        OrderEntity orderEntity = ordersRepository.findByOrderId(orderApprovedEvent.getOrderId());
+
+        if (orderEntity == null) {
+            log.error("Order Id is null");
+            return;
+        }
+
+        orderEntity.setOrderStatus(orderApprovedEvent.getOrderStatus());
+
+        ordersRepository.save(orderEntity);
     }
 }
